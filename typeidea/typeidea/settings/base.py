766bc6d3571c63d84665546f4c5c47dc5c47553e
Dean <elasticbetter@gmail.com>
2019-04-05 14:48:00 +0800

    chapter7 until 7.1.8

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -31,10 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'typeidea',
     'blog',
     'config',
     'comment',
-
+#above are user defined apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

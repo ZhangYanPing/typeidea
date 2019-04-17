aeebeb8ac9bb7076671d3b8e2fd24569be5cfd21
Dean <elasticbetter@gmail.com>
2019-04-17 08:37:44 +0800

    completion of 9.3.5

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -21,6 +21,7 @@ from blog.views import post_list, post_detail
 from config.views import links
 from blog.views import (IndexView, CategoryView, TagView, PostDetailView, SearchView, AuthorView)
 from config.views import LinkListView
+from comment.views import CommentView
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
@@ -28,10 +29,11 @@ urlpatterns = [
     url(r'^search/$', SearchView.as_view(), name='search'),
     url(r'^links/$', LinkListView.as_view(), name='links'),
     url(r'^author/(?P<owner_id>\d+)/$', AuthorView.as_view(), name='author'),
+    url(r'^comment/$', CommentView.as_view(), name='comment'),
     url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
     url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
     url(r'^post/(?P<post_id>\d+).html$', PostDetailView.as_view(), name='post-detail'),
-    url(r'^links/$', links, name='links'),
+    url(r'^links/$', LinkListView.as_view(), name='links'),
     url(r'^super_admin/', admin.site.urls, name='super-admin'),
     url(r'^admin/', custom_site.urls, name='admin'),
 ]
